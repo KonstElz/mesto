28ea4aed1130030f7b267b390f9ec5f22b583468
Konstantin Elizaryev <konstelz@yandex.ru>
2021-05-24 13:10:43 +0300

    fix: дописать стили для попапа

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,26 @@
 const editButton = document.querySelector('.profile__info-edit-button');
 const popup = document.querySelector('.popup');
 const closeIcon = document.querySelector('.popup__close');
-const nameInput = document.querySelector('.popup__name');
-const jobInput = document.querySelector('.popup__job');  
+let nameInput = document.querySelector('.popup__name');
+let jobInput = document.querySelector('.popup__job');
 
 function openForm() {
-  popup.classList.add('.popup_is-opened');
+  popup.classList.add('.popup_opened');
 }
 
 function closeForm() {
-  popup.classList.remove('.popup_is-opened');
+  popup.classList.remove('.popup_opened');
 }
 
 editButton.addEventListener('click', openForm);
 closeIcon.addEventListener('click', closeForm);
 
 function formSubmitHandler (evt) {
-  evt.preventDefault();  
-  document.querySelector('.profile-name').textContent = nameInput;
-  document.querySelector('.profile-job').textContent = jobInput;
+  evt.preventDefault();
+
+  const nameInput = document.querySelector('.profile-name').textContent = nameInput;
+  const jobInput = document.querySelector('.profile-job').textContent = jobInput;
+
   closeForm();
 }
 
