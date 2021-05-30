cbc9c2e2f7d56d432c1510b1cbb352619c5c650f
Konstantin Elizaryev <konstelz@yandex.ru>
2021-05-30 20:32:48 +0300

    fix: внести правки по JS 1 код-ревью (черновик)

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 const editButton = document.querySelector('.profile__info-edit-button');
 const popup = document.querySelector('.popup');
 const closeIcon = document.querySelector('.popup__close');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__job');
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_job');
 
 function openForm() {
+  nameInput.value = document.querySelector('.profile__info-name').textContent;
+  jobInput.value = document.querySelector('.profile__info-job').textContent;
   popup.classList.add('popup_opened');
 }
 
@@ -12,16 +14,16 @@ function closeForm() {
   popup.classList.remove('popup_opened');
 }
 
-editButton.addEventListener('click', openForm);
-closeIcon.addEventListener('click', closeForm);
-
 function formSubmitHandler (evt) {
   evt.preventDefault();
-
-  const nameInput = document.querySelector('.profile-name').textContent = nameInput;
-  const jobInput = document.querySelector('.profile-job').textContent = jobInput;
+  document.querySelector('.profile__info-name').textContent = nameInput.value;
+  document.querySelector('.profile__info-job').textContent = jobInput.value;
 
   closeForm();
 }
 
+let formElement = document.querySelector('.popup__form');
+
 formElement.addEventListener('submit', formSubmitHandler);
+editButton.addEventListener('click', openForm);
+closeIcon.addEventListener('click', closeForm);
